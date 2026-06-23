9e2ca2d4e9bb48ae105e8ea420e5b86fa053e0b1
Varad Patil <142288547+Varadpatil1812@users.noreply.github.com>
2026-06-23 08:41:29 +0530

    Time: 0 ms (100%), Space: 43 MB (37.28%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -1,33 +1,24 @@
 class Solution {
     public int rearrangeCharacters(String s, String target) {
+        int[] countS = new int[26];
+        int[] countTarget = new int[26];
 
-        if(target.length() > s.length()) 
-            return 0;
-
-        int ans = Integer.MAX_VALUE;
-
-        Map<Character,Integer> need = new HashMap<>();
-        Map<Character,Integer> have = new HashMap<>();
-
-        for(char c: s.toCharArray()){
-            have.put(c,have.getOrDefault(c,0)+1);
+        for (char c : s.toCharArray()) {
+            countS[c - 'a']++;
         }
 
-        for(char c: target.toCharArray()){
-            need.put(c,need.getOrDefault(c,0)+1);
+        for (char c : target.toCharArray()) {
+            countTarget[c - 'a']++;
         }
 
-        for(char c: need.keySet()){
-            if(!have.containsKey(c)){
-                return 0;
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 26; i++) {
+            if (countTarget[i] > 0) {
+                answer = Math.min(answer, countS[i] / countTarget[i]);
             }
         }
 
-        for(char c:target.toCharArray()){
-            int fh = have.get(c);
-            int fn = need.get(c);
-            ans = Math.min(fh/fn,ans);
-        }
-        return ans;
-    }   
+        return answer;
+    }
 }
