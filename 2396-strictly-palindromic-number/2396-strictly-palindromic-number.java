59bf2e0ce630863e632be6aeb4e5cd6ed1a07557
Varad Patil <142288547+Varadpatil1812@users.noreply.github.com>
2026-06-26 13:49:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.44%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        boolean ans = true;
-        for (int i = 2; i <= n - 2; i++){
-            if(!isPalindrome(Integer.toString(n,i))){
-                ans = false;
-                break;
-            }
-        }
-        return ans; 
-        
-    }
 
-    public boolean isPalindrome(String s){
-        int left = 0;
-        int right = s.length() - 1;
-        while(left <= right){
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
+        for (int i = 2; i <= n - 2; i++) {
+            String s = Integer.toString(n, i);
+
+            int left = 0;
+            int right = s.length() - 1;
+
+            while (left < right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
             }
-            left++;
-            right--;
         }
 
         return true;
